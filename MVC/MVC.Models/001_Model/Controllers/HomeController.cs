74bd894a3ee3fe46a92e85d377aee46238b55607
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-11-11 14:32:27 +0400

    aa

---
+++ MVC/MVC.Models/001_Model/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _001_Model.Models;
+
+namespace _001_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("{controller}/index/{id?}")]
+        public string Index()
+        {
+            PlainModel model = new PlainModel()
+            {
+                Company = "AppleIndex",
+                Employees = 1000000,
+                Salary = 400000
+            };
+
+            return $"Company: {model.Company}\nEmployees: {model.Employees}\nSalary: {model.Salary}\n";
+
+        }
+
+        public string Home1()
+        {
+            PlainModel model = new PlainModel()
+            {
+                Company = "AppleIndex1",
+                Employees = 2000000,
+                Salary = 800000
+            };
+
+            return $"Company: {model.Company}\nEmployees: {model.Employees}\nSalary: {model.Salary}\n";
+        }
+
+    }
+}
---
+++ MVC/MVC.Models/002_ModelToView/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using _002_ModelToView.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace _002_ModelToView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            PlainModel plainModel = new PlainModel()
+            {
+                Company = "Apple",
+                Employees = 1000000,
+                Salary = 400000
+            };
+
+            ViewDataDictionary valuePairs = new ViewDataDictionary(ViewData);
+            valuePairs.Add("Company", "Apple");
+            valuePairs.Add("Employees", "1000000");
+            valuePairs.Add("Salary", "400000");
+            valuePairs.Add("user", "password");
+            valuePairs.Add("count", valuePairs.Count);
+
+            foreach (var item in valuePairs)
+            {
+                ViewData[item.Key] = item.Value;
+            }
+            return View(valuePairs);
+        }
+    }
+}
